deed06c7cac27752700b8ba694df98813d3a26a0
zhanyao <zhanyao@qq.com>
2022-01-18 16:57:30 +0800

    Separate sequence

--- datastructures_algorithms.py
+++ datastructures_algorithms.py
@@ -1,1 +1,14 @@
+def separate():
+    """Separate the sequence(any iterable object) into individual variables"""
+    data = ["ACME", 50, 91.1, (2012, 12, 21)]
+    name, shares, price, date = data
+    # only need shares and price
+    _, shares, price, _ = date
+    ign, shares, price, ign = date
 
+    # Separate uncertain quantity sequence
+    head, *ign = data
+    *ign, tail = data
+
+    line = "nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false"
+    uname, *fields, homedir, sh = line.split(":")

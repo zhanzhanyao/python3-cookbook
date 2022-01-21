27b30fcf9f45b2b2d69b4dd0c955ce179d0ebd9a
zhanyao <zhanyao@qq.com>
2022-01-21 14:51:15 +0800

    Implement a queue sorted by priority

--- datastructures_algorithms.py
+++ datastructures_algorithms.py
@@ -66,3 +66,34 @@ def learn_heap():
     2. N is near the length of input, use sorted(items)[:N]
     3. Search one largest/smallest using max(),min()
     """
+
+
+def learn_heap_1():
+    """Queue sorted by priority, every pop operation will get the highest priority element"""
+    import heapq
+
+    class PriorityQueue:
+        def __init__(self):
+            self._queue = []
+            self._index = 0
+
+        def push(self, item, priority):
+            heapq.heappush(self._queue, (-priority, self._index, item))
+            self._index += 1
+
+        def pop(self):
+            return heapq.heappop(self._queue)[-1]
+
+    class Item:
+        def __init__(self, name):
+            self.name = name
+
+        def __repr__(self):
+            return "Item({})".format(self.name)
+
+    q = PriorityQueue()
+    q.push(Item("foo"), 1)
+    q.push(Item("bar"), 5)
+    q.push(Item("spam"), 4)
+    q.push(Item("grok"), 1)
+    q.pop()

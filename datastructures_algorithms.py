f356ecf098960d6c1d25701268bda22b144c31c9
zhanzhanyao <940105829@qq.com>
2022-01-23 22:13:29 +0800

    Transform and calculate a sequence meantime

--- datastructures_algorithms.py
+++ datastructures_algorithms.py
@@ -350,6 +350,7 @@ def learn_dicefilter():
 
 def learn_namedtup():
     from collections import namedtuple
+
     Subscriber = namedtuple("Subscriber", ["addr", "joined"])
     sub = Subscriber("940105829@qq.com", "2021/12/12")
     sub.addr  # 940105829@qq.com
@@ -365,9 +366,44 @@ def learn_namedtup():
             return total
 
     # Replace Dict to reduce memory costs
-    Stock = namedtuple("Stock", ["name", "shares", "price","date","time"])
+    Stock = namedtuple("Stock", ["name", "shares", "price", "date", "time"])
     # Create a prototype instance
-    stock_prototype = Stock("",0,0.0,None,None)
-    a = {'name': 'ACME', 'shares': 100, 'price': 123.45}
+    stock_prototype = Stock("", 0, 0.0, None, None)
+    a = {"name": "ACME", "shares": 100, "price": 123.45}
     s = stock_prototype._replace(**a)
     print(s)
+
+
+def learn_generator():
+    """Transform and calculate a sequence simultaneously"""
+    nums = [1, 2, 3, 4, 5]
+    s = sum(n for n in nums)
+
+    # Determine if any .py files exist in a directory
+    import os
+
+    files = os.listdir("dirname")
+    if any(name.endswith(".py") for name in files):
+        print("Python file exists")
+    else:
+        print("No python file")
+
+    # Output a tuple as csv
+    s = ("ACME", 50, 123.45)
+    print(",".join(str(x) for x in s))
+
+    # Data reduction across fields of a data structure
+    portfolio = [
+        {"name": "GOOG", "shares": 50},
+        {"name": "YHOO", "shares": 75},
+        {"name": "AOL", "shares": 20},
+        {"name": "SCOX", "shares": 65},
+    ]
+    min_shares = min(s["shares"] for s in portfolio)
+    min_shares = min(portfolio, key=lambda n: n["shares"])
+
+    s = sum((x * x for x in nums))
+    s = sum(x * x for x in nums)  # more elegant
+    s = sum(
+        [x * x for x in nums]
+    )  # need to create a extra list, if list is very large, will cost many memories

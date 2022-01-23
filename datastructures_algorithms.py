254fab49b3358796d5f706d7e7d64d0ff6178007
zhanzhanyao <940105829@qq.com>
2022-01-23 13:20:53 +0800

    Group dict by desired field

--- datastructures_algorithms.py
+++ datastructures_algorithms.py
@@ -260,3 +260,36 @@ def sort_notcompare():
         from operator import attrgetter
 
         sorted(users, key=attrgetter("user_id"))
+
+
+def group_dict():
+    rows = [
+        {"address": "5412 N CLARK", "date": "07/01/2012"},
+        {"address": "5148 N CLARK", "date": "07/04/2012"},
+        {"address": "5800 E 58TH", "date": "07/02/2012"},
+        {"address": "2122 N CLARK", "date": "07/03/2012"},
+        {"address": "5645 N RAVENSWOOD", "date": "07/02/2012"},
+        {"address": "1060 W ADDISON", "date": "07/02/2012"},
+        {"address": "4801 N BROADWAY", "date": "07/01/2012"},
+        {"address": "1039 W GRANVILLE", "date": "07/04/2012"},
+    ]
+    # Group and access the sequence
+    from itertools import groupby
+    from operator import itemgetter
+    # sort by desired field first because groupby optration work on continuous records
+    # rows = sorted(rows,key=itemgetter("date"))
+    rows.sort(key=itemgetter("date"))
+    # Iterate in groups
+    for date,items in groupby(rows,key=itemgetter("date")):
+        print(date)
+        for i in items:
+            print(i)
+
+    from collections import defaultdict
+    rows_by_date = defaultdict()
+    for row in rows:
+        rows_by_date[row["date"]].append(row)
+
+    for i in rows_by_date["07/01/2012"]:
+        print(i)
+    

2fa259209d11ddb2cfc604798a1c0b3423b32d77
zhanzhanyao <940105829@qq.com>
2022-01-24 16:24:29 +0800

    Text searching and replacing

--- strings_and_text.py
+++ strings_and_text.py
@@ -81,3 +81,33 @@ def match_string_2():
 
     # return a iterator
     datepat.finditer(text1)
+
+
+def replace_text():
+    """Text searching and replacing"""
+    # simple seaching
+    text = 'yeah, but no, but yeah, but no, but yeah'
+    print(text.replace("yeah","yep"))
+
+    # regex searching
+    text = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
+    import re
+    re.sub(r"(\d+)/(\d+)/(\d+)", r"\3-\1-\2", text)  # 'Today is 2012-11-27. PyCon starts 2013-3-13.'
+
+    # pattern
+    datepat = re.compile(r"(\d+)/(\d+)/(\d+)")
+    datepat.sub(r"\3-\1-\2",text)  # 'Today is 2012-11-27. PyCon starts 2013-3-13.'
+
+    # named regex
+    re.sub(r"(?P<month>\d+)/(?P<day>\d+)/(?P<year>\d+)/", r"\g<year>-\g<month>-\g<day>-",text)
+
+    #
+    from calendar import month_abbr
+    def change_date(m):
+        mon_name = month_abbr[int(m.group(1))]
+        return "{}{}{}".format(m.group(2, mon_name, m.group(3)))
+
+    datepat.sub(change_date, text)
+
+
+

c9e5091a1472bf7fa645434c29e6f519fa45e0ae
zhanzhanyao <940105829@qq.com>
2022-01-25 15:17:12 +0800

    Random

--- numbers_dates_time.py
+++ numbers_dates_time.py
@@ -5,5 +5,14 @@ def round_number():
     # format
     format(1.236, "0.2f")  # 1.24
     "value is {:0.2f}".format(1.236)  # value is 1.24
+    # decimal module can make result precise
 
 
+def learn_random():
+    import random
+    values = [1, 2, 3, 4, 5, 6]
+    random.choice(values)
+    random.sample(values, 3)
+    random.shuffle(values)
+    random.randint(0,9)
+    random.random()

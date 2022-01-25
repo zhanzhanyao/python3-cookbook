6e4a87461553646c7a45789cf38b8f0ef9b03b19
zhanzhanyao <940105829@qq.com>
2022-01-25 14:46:26 +0800

    Round number

---
+++ numbers_dates_time.py
@@ -0,0 +1,9 @@
+def round_number():
+    round(1.23, 1)  # 1.2
+    round(1.5, 0)  # 2
+    round(101.5, -1)  # 100.0
+    # format
+    format(1.236, "0.2f")  # 1.24
+    "value is {:0.2f}".format(1.236)  # value is 1.24
+
+

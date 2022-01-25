f529332e0ca07b5320f8492286b89ac53781eeef
zhanzhanyao <940105829@qq.com>
2022-01-25 15:31:17 +0800

    Calculate date and time

--- numbers_dates_time.py
+++ numbers_dates_time.py
@@ -10,9 +10,42 @@ def round_number():
 
 def learn_random():
     import random
+
     values = [1, 2, 3, 4, 5, 6]
     random.choice(values)
     random.sample(values, 3)
     random.shuffle(values)
-    random.randint(0,9)
+    random.randint(0, 9)
     random.random()
+
+
+def convert_date():
+    from datetime import timedelta, datetime
+
+
+def caculate_date():
+    from datetime import datetime, timedelta
+
+    weekdays = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+
+    def get_previous_byday(dayname,start_date=None):
+        if start_date == None:
+            start_date = datetime.now()
+        day_num = start_date.weekday()
+        day_num_target = weekdays.index(dayname)
+        days_ago = (7 + day_num -day_num_target) % 7
+        if days_ago == 0:
+            days_ago = 7
+        target_date = start_date - timedelta(days=days_ago)
+        return target_date
+
+    get_previous_byday("Sunday", datetime(2021,12,21))
+

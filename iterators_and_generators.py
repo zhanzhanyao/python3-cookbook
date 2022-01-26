fa4af910f220d9a4a8e4ec3184f8569f6f966e35
zhanzhanyao <940105829@qq.com>
2022-01-26 17:00:32 +0800

    Example of zip

--- iterators_and_generators.py
+++ iterators_and_generators.py
@@ -194,5 +194,11 @@ def learn_enumerate():
         for word in words:
             word_summary[word].append(idx)
 
+def learn_zip():
+    """iterate over multiple sequences simultaneously"""
+    xpts = [1, 5, 4, 2, 10, 7]
+    ypts = [101, 78, 37, 15, 62, 99]
+    for x, y in zip(xpts,ypts):
+        print(x,y)
 
 

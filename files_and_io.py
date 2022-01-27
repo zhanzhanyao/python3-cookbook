662717c24e7bedbca0b663540c3f040e4dd90da6
zhanzhanyao <940105829@qq.com>
2022-01-27 16:04:54 +0800

    Usage of sep and end in print

--- files_and_io.py
+++ files_and_io.py
@@ -1,24 +1,38 @@
 def read_write():
     """read write text data"""
     # read the entire file as a single string
-    with open("somefile.txt","rt") as f:
+    with open("somefile.txt", "rt") as f:
         f.read()
         f.readlines()
-    # Iterate over the lines of the file
+        # Iterate over the lines of the file
         for line in f:
             print(line)
 
     # Write chunks of text data
-    with open("Output.txt","at") as f:
+    with open("Output.txt", "at") as f:
         f.write("I saw an angle")
-    # Redirected print statement
+        # Redirected print statement
         print("Hello, it's me", file=f)
 
     # if don't use with with statement, need manually close file
-    f = open("somefile.txt","rt")
+    f = open("somefile.txt", "rt")
     data = f.read()
     f.close()
 
+    # print to file
+    with open("test.txt", "wt") as f:
+        print("someone like you", file=f)
+
+
+def learn_print():
+    "Usage of separator and line end"
+    print("a", 1, 2, 3)
+    print("a", 1, 2, 3, sep=",")
+    print("a", 1, 2, 3, end="!!\n")
+
+    # print in one line
+    for i in range(5):
+        print(i, end=" ")
 
 
 

b2598e73dcab7c2edc3bd97d151caf0232ed645e
zhanzhanyao <940105829@qq.com>
2022-02-05 21:33:08 +0800

    access and modify the internal variable of the closure function

--- functions.py
+++ functions.py
@@ -112,3 +112,30 @@ def learn_callback():
         return x + y
 
     apply_async(add, (2, 3), callback=print_result)
+
+
+# skip 7-10, 7-11 because it's not easy for me to understand at this moment
+def learn_closure1():
+    """access and modify the internal variable of closure function"""
+    def sample():
+        n = 0
+        # closure function
+        def func():
+            print("n=", n)
+
+        def get_n():
+            return n
+
+        def set_n(value):
+            nonlocal n
+            n = value
+
+        func.get_n = get_n
+        func.set_n = set_n
+        return func
+
+    f = sample()
+    print(f())
+    f.set_n(10)
+    print(f())
+    print(f.get_n())

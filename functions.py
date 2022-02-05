e658004fa72cb23d191b5c5cb1fa0f02d0a8fc62
zhanzhanyao <940105829@qq.com>
2022-02-05 21:01:57 +0800

    default params and matadata of function & usage of closure

--- functions.py
+++ functions.py
@@ -39,3 +39,60 @@ def learn_params_1():
         if clip is not None:
             m = clip if clip > m else m
             return m
+
+
+def func_matadata():
+    """attach informatinal matadata to function parameters"""
+
+    def add(x: int, y: int) -> int:
+        return x + y
+
+    help(add)
+
+
+def default_params():
+    """
+    define functions with default arguments
+    default parameters must be immutable types.
+    """
+
+    def spam(a, b=42):
+        # param is a value
+        print(a, b)
+
+    def spam1(a, b=None):
+        # param is a list
+        if b is None:
+            b = []
+
+    # check if a desired default parameter is inputted
+    _no_value = object()
+
+    def spam2(a, b=_no_value):
+        if b is _no_value:
+            print("No b value supplied")
+
+
+def learn_closure():
+    from urllib.request import urlopen
+
+    class UrlTemplate:
+        def __init__(self, template):
+            self.template = template
+
+        def open(self, **kwargs):
+            return urlopen(self.template.format_map(kwargs))
+
+    yahoo = UrlTemplate("http://finance.yahoo.com/d/quotes.csv?s={names}&f={fields}")
+    for line in yahoo.open(names="IBM,AAPL,FB", fields="sl1clv"):
+        print(line.decode("utf-8"))
+
+    def urltemplate(template):
+        def opener(**kwargs):
+            return urlopen(template.format_map(kwargs))
+
+        return opener
+
+    yahoo = urltemplate("http://finance.yahoo.com/d/quotes.csv?s={names}&f={fields}")
+    for line in yahoo(names="IBM,AAPL,FB", fields="sl1clv"):
+        print(line.decode("utf-8"))

e7615acef4113e3e8bf2f2ee313febfa989e77ad
zhanzhanyao <940105829@qq.com>
2022-02-06 23:47:33 +0800

    Usage of context management protocol & with statement

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -42,3 +42,58 @@ def instance_format():
     print(format(d, "mdy"))
     print(format(d, "dmy"))
     print("This day is {:dmy}".format(d))
+
+
+def learn_with():
+    """make objects support context management protocol"""
+    from socket import socket, AF_INET, SOCK_STREAM
+
+    class LazyConnection:
+        def __init__(self, address, family=AF_INET, type=SOCK_STREAM):
+            self.address = address
+            self.family = family
+            self.type = type
+            self.sock = None
+
+        def __enter__(self):
+            if self.sock is not None:
+                raise RuntimeError("Already connected")
+            self.sock = socket(self.family, self.type)
+            self.sock.connect(self.address)
+            return self.sock
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self.sock.close()
+            self.sock = None
+
+    from functools import partial
+
+    conn = LazyConnection(("www.python.org", 80))
+
+    with conn as s:
+        s.send(b"GET /index.html HTTP/1.0\r\n")
+        s.send(b"Host: www.python.org\r\n")
+        s.send(b"\r\n")
+        resp = b"".join(iter(partial(s.recv, 8192), b""))
+
+    class LazyConnection1:
+        def __init__(self, address, family=AF_INET, TYPE=SOCK_STREAM):
+            self.address = address
+            self.family = family
+            self.type = type
+            self.connections = []
+
+        def __enter__(self):
+            sock = socket(self.family, self.type)
+            sock.connect(self.address)
+            self.connections.append(sock)
+            return sock
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self.connections.pop().close()
+
+    conn = LazyConnection1(("www.python.org", 80))
+    with conn as s1:
+        pass
+        with conn as s2:
+            pass

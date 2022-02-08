b39cf87aace0bbb0d363586f5f744497aba49462
zhanzhanyao <940105829@qq.com>
2022-02-09 00:22:50 +0800

    implementing data model or type system

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -276,3 +276,72 @@ def learn_abstract_class():
         def write(self, data):
             pass
 
+
+def imple_datamodel():
+    """implementing data model or type system"""
+    class Descriptor:
+        # Base class. uses a descriptor to set a value
+        def __init__(self, name=None, **opts):
+            self.name = name
+            for key, value in opts.items():
+                setattr(self, key, value)
+
+        def __set__(self, instance, value):
+            instance.__dict_[self.name] = value
+
+
+    # Descriptor for enforcing types
+    class Typed(Descriptor):
+        expected_type = type(None)
+
+        def __set__(self, instance, value):
+            if not isinstance(value, self.expected_type):
+                raise TypeError("expected" + str(self.expected_type))
+            super().__set__(instance, value)
+
+    # Descriptor for enforcing values
+    class Unsigned(Descriptor):
+        def __set__(self, instance, value):
+            if value < 0:
+                raise ValueError("Expected < 0")
+            super().__set__(instance, value)
+
+    class MaxSize(Descriptor):
+        def __init__(self, name=None, **opts):
+            if "size" not in opts:
+                raise TypeError("missing size option")
+            super().__init__(name, **opts)
+
+        def __set__(self, instance, value):
+            if len(value) >= self.size:
+                raise ValueError("size must be <" + str(self.size))
+            super.__set__(instance, value)
+
+    class Integer(Typed):
+        expected_type = int
+
+    class UnsignedInteger(Integer, Unsigned):
+        pass
+
+    class Float(Typed):
+        expected_type = float
+
+    class UnsignedFloat(Float, Unsigned):
+        pass
+
+    class String(Typed):
+        expected_type = str
+
+    class SizedString(String, MaxSize):
+        pass
+
+    class Stock:
+        # specify constraints
+        name = SizedString("name", size=8)
+        shares = UnsignedInteger("shares")
+        price = UnsignedFloat("price")
+
+        def __init__(self, name, shares, price):
+            self.name = name
+            self.shares = shares
+            self.price = price

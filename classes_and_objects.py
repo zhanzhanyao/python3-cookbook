211342b8b1fa5b8952a8a5cc083bc8edd4b59650
zhanzhanyao <940105829@qq.com>
2022-02-08 23:35:27 +0800

    Update simplify initialization of data structure

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -232,3 +232,28 @@ def simp_init():
     p = Point(2, 3)
     c = Circle(4.5)
     S2 = Stock("ACME", 50)  # TypeError: Expected 3 arguments
+
+    class Structures2():
+        _fields = []
+
+        def __init__(self, *args, **kwargs):
+            if len(args) > len(self._fields):
+                raise TypeError("Expected {} arguments".format(len(self._fields)))
+
+            # set all of the positional arguments
+            for name, value in zip(self._fields, args):
+                setattr(self, name, value)
+
+            # set the remaining keyword arguments
+            for name in self._fields[len(args)]:
+                setattr(self, name, kwargs.pop(name))
+
+            # check for any remaining unknown arguments
+            if kwargs:
+                raise TypeError("Invalid argument(s): {}".format(",".join(kwargs)))
+
+    s1 = Stock('ACME', 50, 91.1)
+    s2 = Stock('ACME', 50, price=91.1)
+    s3 = Stock('ACME', shares=50, price=91.1)
+
+

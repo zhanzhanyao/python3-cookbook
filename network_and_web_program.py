d17d3749efd046ce37b4f10305d5ca2cf000a1d3
zhanzhanyao <940105829@qq.com>
2022-02-12 00:48:51 +0800

    Execute functions or methods running on remote server

--- network_and_web_program.py
+++ network_and_web_program.py
@@ -165,3 +165,48 @@ def learn_rest():
     httpd = make_server("", 8080, dispatcher)
     print("Serving on port 8080...")
     httpd.serve_forever()
+
+
+def learn_xml_rpc():
+    """implement simple remote procedure call with xml_rpc"""
+    # server
+    from xmlrpc.server import SimpleXMLRPCServer
+
+    class KeyValueServer:
+        _rpc_methods_ = ["get", "set", "delete", "exists", "keys"]
+
+        def __init__(self, address):
+            self._data = {}
+            self._serv = SimpleXMLRPCServer(address, allow_none=True)
+            for name in self._rpc_methods_:
+                self._serv.register_function(getattr(self, name))
+
+        def get(self, name):
+            return self._data[name]
+
+        def set(self, name, value):
+            self._data[name] = value
+
+        def delete(self, name):
+            del self._data[name]
+
+        def exists(self, name):
+            return name in self._data
+
+        def keys(self):
+            return list(self._data)
+
+        def serve_forever(self):
+            self._serv.serve_forever()
+
+    kvserv = KeyValueServer(("", 15000))
+    kvserv.serve_forever()
+
+    # client
+    from xmlrpc.client import ServerProxy
+
+    s = ServerProxy("http://localhost:15000", allow_none=True)
+    s.set("foo", "bar")
+    s.set("spam", [1, 2, 3, 4])
+    s.keys()
+    s.get("foo")
